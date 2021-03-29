defe05e1407123886d7240bf84e73daccaea3800
kj-ninja <jakubiak.krzy@gmail.com>
2021-03-29 12:13:17 +0200

    added basic structure like User and Company class also added google maps to the project.

---
+++ src/User.ts
@@ -0,0 +1,18 @@
+import * as faker from 'faker';
+
+export class User {
+  name: string
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: +faker.address.latitude(),
+      lng: +faker.address.longitude(),
+    };
+  }
+}
+
